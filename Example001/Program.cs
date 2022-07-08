d3971dc4ab24b8c57d6b37cf80f120e7f68c5ec4
SeRozhka <marphiy@gmail.com>
2022-07-08 22:36:55 +0300

    Added Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Hello, World!, Enter your name!: ");
+string username = Console.ReadLine();
+if (username.ToUpper() == "JOHN")
+{
+    Console.Write("Hello, my dear! How are you, John?");
+}
+else 
+{
+    Console.Write("Nice to meet you!");
+}
